2261fd4f6cffa870e1fe4f20cc657d321b277015
mrshodiyor <shodiyoribrohimjonov2004@gmail.com>
2023-07-18 11:31:32 -0700

    change hometaskN1

--- hometaskN1/Program.cs
+++ hometaskN1/Program.cs
@@ -11,3 +11,15 @@ Console.Clear();
 DateTime time = DateTime.Now;
 
 Console.WriteLine($"-> {time}\n-> {name}\n-> {secondName}\n-> {age}\n-> 125.4 $");
+
+
+// value type
+int a = 12;
+// reference type 
+string b = "Hello World";
+//primitive type
+bool c = false;
+//Complex type
+object d = null;
+
+//change1111111111111

2fce4169d347111eb1838cab61097d77813098c3
mrshodiyor <shodiyoribrohimjonov2004@gmail.com>
2023-07-18 11:28:43 -0700

    change

---
+++ N6/Program.cs
@@ -0,0 +1,52 @@
+﻿#region Operators
+
+// Arithmetic operatorlar - arifmetik
+
+// Unary
+
+// Increment
+// var forIncrement = 1
+
+// Post Increment
+// Console.WriteLine(forIncrement++)
+
+// Pre Increment
+// console.WriteLine(++forIncrement)
+
+//Decrement
+
+// Post Decrement
+// Consloe.WriteLine(forIncrement--)
+
+// Pre Decrement
+// Console.WriteLine(--forIncrement
+
+// Minus
+// --forIncrement;
+// --forIncrement;
+// --forIncrement;
+// Consle.WriteLine(-forIncrement);
+// Conslole..Writeline(forIncrement);
+
+// Binary
+// var a = 10 + 10;
+// var b = 10 - 10;
+//var c = 10 * 10;
+//var d = 11F / 10;
+//var e = 10 % 10;
+
+// Assignment ooperatorlar - tenglash
+//int f, h, g;
+//f = h = g = 10;
+
+// Operator with assignment
+//f += 10 + h;
+//f -= 10;
+//f *= 10;
+//f /= 10;
+//f %= 10;
+
+// Boolean eperators - mantiqiy
+
+// !
+//Console.WriteLine(!True\)
---
+++ hometaskN1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+
+Console.Write("Ismingizni kiriting: ");
+string name = Console.ReadLine();
+Console.Write("Familiyangizni kiriting: ");
+string secondName = Console.ReadLine();
+Console.Write("yoshingizni kiriting: ");
+int age = int.Parse( Console.ReadLine());
+
+Console.Clear();
+DateTime time = DateTime.Now;
+
+Console.WriteLine($"-> {time}\n-> {name}\n-> {secondName}\n-> {age}\n-> 125.4 $");
